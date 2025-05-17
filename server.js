1b3fd9a28f8f80f2254a6d9e3a57faa9780ae590
amorepi <piero@ingamore.it>
2025-05-18 00:38:24 +0200

    Variazione messaggio dal MacBookAir

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5500;
 app.get('/', (req, res) => {
     res.send(`<p>url: <a href="https://ingamore.net">ingamore.net</a></p>\
               <p>url: <a href="https://www.ingamore.net">www.ingamore.net</a></p>\
-              <p>Hello World! Ciao a tutti</p>\
+              <p>Hello World!</p>\
               <p>Node Versione: ${process.version}</p>\
               <p>datatime: ${new Date().toLocaleString('it-IT', {timeZone: 'Europe/Rome', hour12: false})}</p>\
               `);

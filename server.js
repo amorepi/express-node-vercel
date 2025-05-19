77aafd4186e77d1e38d648ba8b728f2c7a5dc84b
amorepi <piero@ingamore.it>
2025-05-19 12:42:53 +0200

    update MacBookAir

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
     res.send(`<p>url: <a href="https://ingamore.net">ingamore.net</a></p>\
               <p>url: <a href="https://www.ingamore.net">www.ingamore.net</a></p>\
               <p>Hello World!</p>\
+              <p>dirname: ${__dirname}</p>\
               <p>Node Versione: ${process.version}</p>\
               <p>datatime: ${new Date().toLocaleString('it-IT', {timeZone: 'Europe/Rome', hour12: false})}</p>\
               `);
